57cc0dc13f86e4cf2081db1687632aecfc2c27b9
Arlindo Garcia <arlindogarciagomesneto70@gmail.com>
2024-07-27 14:03:54 -0300

    Feat: nao deixar dar zoom

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="pt">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="shortcut icon" href="/logo-white.png" type="image/x-icon" />
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
@@ -12,6 +12,22 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
+        <style>
+            input[type="text"]:focus {
+                transform: scale(1);
+            }
+        </style>
+        <script>
+            document.querySelectorAll('input[type="text"]').forEach(input => {
+                input.addEventListener('focus', () => {
+                    document.body.style.zoom = "1";
+                });
+
+                input.addEventListener('blur', () => {
+                    document.body.style.zoom = "1";
+                });
+            });
+        <script>
         <!-- Scripts -->
         @routes
         @viteReactRefresh
